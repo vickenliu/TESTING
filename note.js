a1161485af42e4c53e415c0bd4dfc5d1f8645d19
Vicken <vicken.liu@gmail.com>
2018-01-23 21:34:33 +1300

    Create note.js

---
+++ note.js
@@ -0,0 +1,7 @@
+### new react route.
+
+react-router-dome has 'staticRouter' and 'browserRouter' to wrap the App
+
+for react to be rendered on the server, staticRouter is used, it accepts location(url) and context (the props for the matched component)
+
+//To do: update the reddit-wlg's server reder routed react page.
